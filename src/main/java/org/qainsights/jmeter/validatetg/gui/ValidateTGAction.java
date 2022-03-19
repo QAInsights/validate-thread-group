193a71996bf0e867733130c6fc1a5eda1865ed70
Leela Prasad <47483946+leelaprasadv@users.noreply.github.com>
2022-03-19 15:07:11 +0530

    Shortcut keys support and Icon adjustments

--- src/main/java/org/qainsights/jmeter/validatetg/gui/ValidateTGAction.java
+++ src/main/java/org/qainsights/jmeter/validatetg/gui/ValidateTGAction.java
@@ -1,18 +1,27 @@
 package org.qainsights.jmeter.validatetg.gui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import javax.swing.AbstractAction;
 
 class ValidateTGAction extends AbstractAction {
 
+    public static final KeyStroke VALIDATE_ALL_TG   = KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.ALT_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK);
+    private static final Logger log = LoggerFactory.getLogger(ValidateTGAction.class);
+
     ValidateTGAction() {
-        super("Validate Thread Group");
+        super("Validate Thread Group(s)");
         putValue(Action.ACTION_COMMAND_KEY, "validate_tg");
+        putValue(Action.ACCELERATOR_KEY, VALIDATE_ALL_TG);
+        putValue(Action.SMALL_ICON, ValidateTGMenuItem.getButtonIcon(12));
     }
 
     public void actionPerformed(ActionEvent actionEvent) {
-        System.out.println("");
+        log.debug("Validate Thread Groups Action performed from Run menu!");
     }
 }
