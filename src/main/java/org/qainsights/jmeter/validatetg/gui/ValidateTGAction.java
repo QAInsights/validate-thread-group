d93a5757fe60fb96acc24bc447746bab7cfd4358
Leela Prasad <47483946+leelaprasadv@users.noreply.github.com>
2022-03-19 15:44:23 +0530

    Updated shortcut key to ALT + V

--- src/main/java/org/qainsights/jmeter/validatetg/gui/ValidateTGAction.java
+++ src/main/java/org/qainsights/jmeter/validatetg/gui/ValidateTGAction.java
@@ -11,13 +11,13 @@ import javax.swing.AbstractAction;
 
 class ValidateTGAction extends AbstractAction {
 
-    public static final KeyStroke VALIDATE_ALL_TG   = KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.ALT_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK);
+    public static final KeyStroke VALIDATE_TG   = KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.ALT_DOWN_MASK);
     private static final Logger log = LoggerFactory.getLogger(ValidateTGAction.class);
 
     ValidateTGAction() {
         super("Validate Thread Group(s)");
         putValue(Action.ACTION_COMMAND_KEY, "validate_tg");
-        putValue(Action.ACCELERATOR_KEY, VALIDATE_ALL_TG);
+        putValue(Action.ACCELERATOR_KEY, VALIDATE_TG);
         putValue(Action.SMALL_ICON, ValidateTGMenuItem.getButtonIcon(12));
     }
 
